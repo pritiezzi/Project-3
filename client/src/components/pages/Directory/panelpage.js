925c39c23d56e64b3db3b20c7de2f9c0213e95c3
priscila <pristiezzi@gmail.com>
2019-10-03 18:51:00 -0500

    box changes

--- client/src/components/pages/Directory/panelpage.js
+++ client/src/components/pages/Directory/panelpage.js
@@ -1,48 +1,52 @@
 import React from 'react';
-import { MDBCard, MDBCardBody,MDBCol, MDBRow, MDBContainer  } from 'mdbreact';
-
-
+import { MDBCard, MDBCardBody, MDBCol, MDBRow, MDBContainer } from "mdbreact";
 
 const PanelPage = () => {
-    return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6" className="mb-4">
-          <MDBCard color="indigo" text="white" className="text-center">
-            <MDBCardBody>
-              Very interesting conversation
-            </MDBCardBody>
-          </MDBCard>
-    
-          <MDBCard color="pink lighten-2" text="white" className="text-center">
-            <MDBCardBody>
-              Great asset for future business
-            </MDBCardBody>
-          </MDBCard>
-    
-          <MDBCard color="info-color" text="white" className="text-center">
-            <MDBCardBody>
-            Alert! Please don't forget to contact 
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-    
-        <MDBCol md="6">
-          <MDBCard color="red lighten-1" text="white" className="text-center">
-            <MDBCardBody>
-              Think twice before contacting 
-            </MDBCardBody>
-          </MDBCard>
-    
-          <MDBCard color="mdb-color lighten-2" text="white" className="text-center">
-            <MDBCardBody>
-              Don't ever contact
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-    );
-    };
-    
-    export default PanelPage;
+return (
+<MDBContainer>
+  <MDBRow>
+    <MDBCol md="6" className="mb-4">
+      <MDBCard color="indigo" text="white" className="text-center">
+        <MDBCardBody>
+         Very intersesting contact/ conversation.
+        </MDBCardBody>
+      </MDBCard>
+
+      <MDBCard color="pink lighten-2" text="white" className="text-center">
+        <MDBCardBody>
+          Great asset for future business.       
+          </MDBCardBody>
+      </MDBCard>
+
+      <MDBCard color="info-color" text="white" className="text-center">
+        <MDBCardBody>
+          Keep in touch! You never know.
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+
+    <MDBCol md="6">
+      <MDBCard color="red lighten-1" text="white" className="text-center">
+        <MDBCardBody>
+          Alert! Don't forget to reach out.
+        </MDBCardBody>
+      </MDBCard>
+
+      <MDBCard color="success-color" text="white" className="text-center">
+        <MDBCardBody>
+          Think twice before contacting.
+        </MDBCardBody>
+      </MDBCard>
+
+      <MDBCard color="mdb-color lighten-2" text="white" className="text-center">
+        <MDBCardBody>
+          No, definatly no. Block. Run.
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+);
+};
+
+export default PanelPage;

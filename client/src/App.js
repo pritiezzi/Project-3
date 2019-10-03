8806f37b1d6980773a63081130f475ac77621249
priscila <pristiezzi@gmail.com>
2019-10-03 16:18:16 -0500

    pushing extra changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./components/pages/Home";
+// import Home from "./components/pages/Home";
 import Profile from "./components/pages/Profile";
-import Directory from "./components/pages/Directory";
+import Directory from ".components/pages/Directory"
 import NavTabs from './components/pages/NavTabs';
 
 
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/" component={Home} /> */}
         <Route exact path="/about" component={Profile} />
         <Route exact path="/blog" component={Directory} />
       </div>
---
+++ client/src/components/pages/Directory/Tablepage.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { MDBCard, MDBCardBody, MDBCardHeader, MDBInput, MDBBtn, MDBTable, MDBTableBody, MDBTableHead,MDBCol, MDBRow, MDBContainer  } from 'mdbreact';
+
+const TablePage = (props) => {
+  const data_panel = {
+    columns: [
+      {
+        'label': <MDBInput label=" " type="checkbox" id="checkbox5" />,
+        'field': 'check',
+        'sort': 'asc'
+      },
+      {
+        'label': 'First Name',
+        'field': 'first',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Last Name',
+        'field': 'last',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Location',
+        'field': 'Location',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Title',
+        'field': 'Title',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Github',
+        'field': 'Github',
+        'sort': 'asc'
+      },
+      {
+        'label': 'Linkedin',
+        'field': 'Linkedin',
+        'sort': 'asc'
+      }
+    ],
+    rows: [
+      {
+        'check': <MDBInput label=" " type="checkbox" id="checkbox6" />,
+        'first': 'Priscila',
+        'last': 'Tiezzi',
+        'Location': 'Nsshville',
+        'Title': 'Front-End developer',
+        'Github': 'https://github.com/pritiezzi',
+        'Linkedin': 'https://www.linkedin.com/in/priscila-tiezzi-927a18165/'
+      },
+      {
+        'check': <MDBInput label=" " type="checkbox" id="checkbox7" />,
+        'first': 'Priscila',
+        'last': 'Tiezzi',
+        'Location': 'Nsshville',
+        'Title': 'Front-End developer',
+        'Github': 'https://github.com/pritiezzi',
+        'Linkedin': 'https://www.linkedin.com/in/priscila-tiezzi-927a18165/'
+      },
+      {
+        'check': <MDBInput label=" " type="checkbox" id="checkbox8" />,
+        'first': 'Priscila',
+        'last': 'Tiezzi',
+        'Location': 'Nsshville',
+        'Title': 'Front-End developer',
+        'Github': 'https://github.com/pritiezzi',
+        'Linkedin': 'https://www.linkedin.com/in/priscila-tiezzi-927a18165/'
+      },
+      {
+        'check': <MDBInput label=" " type="checkbox" id="checkbox9" />,
+        'first': 'Priscila',
+        'last': 'Tiezzi',
+        'Location': 'Nsshville',
+        'Title': 'Front-End developer',
+        'Github': 'https://github.com/pritiezzi',
+        'Linkedin': 'https://www.linkedin.com/in/priscila-tiezzi-927a18165/'
+      },
+      {
+        'check': <MDBInput label=" " type="checkbox" id="checkbox10" />,
+        'first': 'Priscila',
+        'last': 'Tiezzi',
+        'Location': 'Nsshville',
+        'Title': 'Front-End developer',
+        'Github': 'https://github.com/pritiezzi',
+        'Linkedin': 'https://www.linkedin.com/in/priscila-tiezzi-927a18165/'
+      }
+    ]
+  };
+  
+  export default TablePage;
